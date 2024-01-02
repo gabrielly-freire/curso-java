8a18a4d20080a3de5273039e5d1480ecdc1b126b
Gabrielly Freire <gabylopesfreire@gmail.com>
2024-01-02 20:42:27 -0300

    adição de exercicios da seção #7

---
+++ src/main/java/section07/Class57.java
@@ -0,0 +1,14 @@
+package section07;
+
+public class Class57 {
+    public static void main(String[] args) {
+
+        // operadores bitwise - usando em programação em baixo nível
+        int x = 84;
+        int y = 60;
+
+        System.out.println(x & y); // AND
+        System.out.println(x | y); // OR
+        System.out.println(x ^ y); // XOR
+    }
+}
